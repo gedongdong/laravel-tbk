cfb9e5a4a741c75b31e666b247841e13753a623b
gedongdong <gedongdong2010@163.com>
2020-03-27 17:41:31 +0800

    记录user-agent

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,12 +8,21 @@ namespace App\Http\Controllers;
 
 
 use App\Models\Product;
+use App\Models\UserAgent;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index()
     {
+        $agent = request()->header()['user-agent'][0];
+        if ($agent) {
+            $userAgent = new UserAgent();
+
+            $userAgent->user_agent = $agent;
+            $userAgent->save();
+        }
+
         $products = Product::where('status', 1)->whereNotNull('coupon_info')->where('coupon_end_time', '>', date('Y-m-d'))->orderBy('volume', 'desc')->paginate(20);
         return view('index', ['products' => $products]);
     }
@@ -31,7 +40,7 @@ class IndexController extends Controller
 
         $html = '';
         foreach ($products['data'] as $pro) {
-            $html .= '<a href="javascript:void(0)" onclick="show('.$pro['id'].')" class="weui-media-box weui-media-box_appmsg">';
+            $html .= '<a href="javascript:void(0)" onclick="show(' . $pro['id'] . ')" class="weui-media-box weui-media-box_appmsg">';
             $html .= '<div class="weui-media-box__hd" style="height: 5.3rem;width: 5.3rem;line-height: 5.3rem;">';
             $html .= '<img class="weui-media-box__thumb" src="' . $pro['pict_url'] . '">';
             $html .= '</div>';
@@ -55,16 +64,16 @@ class IndexController extends Controller
             $html .= '<p class="weui-media-box__desc" style="margin-top: 0.2rem;">';
             $html .= '<p style="display: block;float: left;line-height: 0.8rem; padding:0 0.2rem 0 0;color:#999;font-size: 0.6rem;">券后 <span style="color:#666;font-weight: bold;font-size: 0.8rem;">¥' . ($pro['zk_final_price'] - $pro['coupon_price']) . '</span></p>';
             $html .= '<p style="display: block;float: left;line-height: 0.8rem; padding:0 0.2rem;color:#999;font-size: 0.6rem;text-decoration: line-through;">￥' . $pro['zk_final_price'] . '</p></p></div></a>';
-            $html .= '<div id="tkl_'.$pro['id'].'" style="background-color: #eee;width: 100%;height: auto;font-size: 0.5rem;color:#666;display: none;">';
+            $html .= '<div id="tkl_' . $pro['id'] . '" style="background-color: #eee;width: 100%;height: auto;font-size: 0.5rem;color:#666;display: none;">';
             $html .= '<div style="padding: 0.6rem;">';
             $html .= '<div style="padding: 0.5rem;">';
-            $html .= '<div>'.$pro['title'].'</div>';
-            $html .= '<div>【在售价】'.$pro['zk_final_price'].'</div>';
-            $html .= '<div>【券后价】'.($pro['zk_final_price']-$pro['coupon_price']).'</div>';
+            $html .= '<div>' . $pro['title'] . '</div>';
+            $html .= '<div>【在售价】' . $pro['zk_final_price'] . '</div>';
+            $html .= '<div>【券后价】' . ($pro['zk_final_price'] - $pro['coupon_price']) . '</div>';
             $html .= '<div>------------</div>';
-            $html .= '<div>注意：请完整复制这条信息，'.$pro['tkpwd'].'，到【手机淘宝】即可查看或分享给好友</div>';
-            $html .= '<input id="txt_'.$pro['id'].'" type="hidden" value="'.$pro['title'].'----【在售价】'.$pro['zk_final_price'].'元----【券后价】'.($pro['zk_final_price']-$pro['coupon_price']).'元----注意：请完整复制这条信息，'.$pro['tkpwd'].'，到【手机淘宝】即可查看或分享给好友"></div>';
-            $html .= '<div style="text-align: right;"  onclick="copyText('.$pro['id'].')">';
+            $html .= '<div>注意：请完整复制这条信息，' . $pro['tkpwd'] . '，到【手机淘宝】即可查看或分享给好友</div>';
+            $html .= '<input id="txt_' . $pro['id'] . '" type="hidden" value="' . $pro['title'] . '----【在售价】' . $pro['zk_final_price'] . '元----【券后价】' . ($pro['zk_final_price'] - $pro['coupon_price']) . '元----注意：请完整复制这条信息，' . $pro['tkpwd'] . '，到【手机淘宝】即可查看或分享给好友"></div>';
+            $html .= '<div style="text-align: right;"  onclick="copyText(' . $pro['id'] . ')">';
             $html .= '<a href="javascript:void(0);" class="weui-btn weui-btn_mini weui-btn_primary">点击复制淘口令</a></div></div></div>';
         }
 
